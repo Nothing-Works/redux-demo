2f743f0e7858fed2c46bd6789e407e28e32d0c85
Andy Song <andylauszp@163.com>
2018-12-06 14:50:31 +1300

    config route/

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import styles from '../assets/jss/Main';
 import { RouteWithSubRoutes, routes } from '../route';
+import { Switch, Redirect } from 'react-router-dom';
 
 const Main = () => {
   return (
     <div>
-      {routes.map((route, i) => (
-        <RouteWithSubRoutes key={i} {...route} />
-      ))}
+      <Switch>
+        {routes.map((route, i) => (
+          <RouteWithSubRoutes key={i} {...route} />
+        ))}
+        <Redirect from="/" to="/dashboard" />
+      </Switch>
     </div>
   );
 };
